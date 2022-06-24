5479357630c24f6ec580c548da60fa743210ab48
tero <kmaniaen@gmail.com>
2022-06-24 13:43:35 +0900

    Add : styled-components 적용

--- src/pages/item/ItemDetail.jsx
+++ src/pages/item/ItemDetail.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import styled from 'styled-components'
+
+// styled-components 적용
+// 단점 : html의 구조가 길어지면 구분이 모호애짐,
+// 스타일 재사용시 import 가능한데 css파일 쓰는거랑 차이점이 없음
+let YellowBtn = styled.button`
+  background : ${ props => props.bg};
+  color : ${ props => props.bg == 'blue' ? 'white' : 'black'};
+  padding: 10px;
+`
+
+let BlackBox = styled.div`
+  background : gray;
+  color : black;
+  padding: 10px;
+` 
 
 export default function ItemDetail({shoes}) {
   let navigate = useNavigate()
@@ -11,6 +27,10 @@ export default function ItemDetail({shoes}) {
 
   return (
     <div className="container text-center">
+        <BlackBox>
+          <YellowBtn bg="orange">버튼</YellowBtn>
+          <YellowBtn bg="blue">버튼</YellowBtn>
+        </BlackBox>
         <div className="row">
             <div className="col-md-6">
                 <img src={`https://codingapple1.github.io/shop/shoes${ Number(id)+1 }.jpg`} width="100%" />

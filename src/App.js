73b59022936a028728f486ae3b2d37c88bf31cde
tero <kmaniaen@gmail.com>
2022-06-24 17:04:43 +0900

    Mod: axios 적용

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import ItemDetail from './pages/item/ItemDetail';
 import Event from './pages/event/Event';
 import { useState } from 'react';
 import data from './db/data'
+import axios from 'axios'
 
 function App() {
-  let [shoes] = useState(data) 
+  let [shoes, setShoes] = useState(data) 
 
   return (
     <div>
@@ -17,8 +18,8 @@ function App() {
       <div className='main-bg'/>
       <Routes>
         <Route path='/' element={<Item shoes={shoes} />}/>
-        <Route path='/detail/:id' element={<ItemDetail shoes={shoes} />}/>
         <Route path='*' element={<div>없는페이지입니다.</div>}/>
+        <Route path='/detail/:id' element={<ItemDetail shoes={shoes} />}/>
 
         {/* nested routed */}
         <Route path='/event' element={<Event />}>
@@ -26,6 +27,15 @@ function App() {
           <Route path='two' element={<div>생일기념 쿠폰 받기</div>}/>
         </Route>
       </Routes>
+      <button onClick={()=>{
+        axios.get('https://codingapple1.github.io/shop/data2.json')
+          .then(res => {
+            // 배열을 합칠때 ... 은 [] 를 벗기는 용도? 사용
+            let newData = [...shoes, ...res.data];
+            setShoes(newData)
+          })
+          .catch(error => console.log(error))
+      }}>가져오기</button>
     </div>
   );
 } 

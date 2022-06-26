0fbf4f8615b8390c09065fd2e8332d7808b972a2
tero <kmaniaen@gmail.com>
2022-06-26 22:26:18 +0900

    Mod: lazy loading 적용

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavComponent from './component/common/NavComponent';
 import Item from './pages/item/Item';
 import { Route, Routes } from 'react-router-dom'
-import ItemDetail from './pages/item/ItemDetail';
 import Event from './pages/event/Event';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, lazy, Suspense } from 'react';
 import data from './db/data'
-import Cart from './pages/cart/Cart';
+
+// lazy 적용
+// import ItemDetail from './pages/item/ItemDetail';
+// import Cart from './pages/cart/Cart';
+
+const ItemDetail = lazy(() => import('./pages/item/ItemDetail'))
+const Cart = lazy(() => import('./pages/cart/Cart'))
 
 function App() {
   let [shoes] = useState(data) 
@@ -21,11 +26,8 @@ function App() {
   
   useEffect(()=>{
     // 로컬스토리지 초기값 셋팅
-    console.log('localStorage');
-    //const local = localStorage.getItem('watched')
     const data = JSON.parse(localStorage.getItem('watched'))
-    console.log(data);
-    console.log(data.length);
+    // 기존 확인 내역이 있으면 유지
     if(!data) localStorage.setItem('watched', JSON.stringify([]))
   },[])
 
@@ -33,17 +35,20 @@ function App() {
     <div>
       <NavComponent />
       <div className='main-bg'/>
-      <Routes>
-        <Route path='/' element={<Item shoes={shoes} />}/>
-        <Route path='*' element={<div>없는페이지입니다.</div>}/>
-        <Route path='/detail/:id' element={<ItemDetail shoes={shoes} />}/>
-        <Route path='/cart' element={<Cart />}/>
-        {/* nested routed */}
-        <Route path='/event' element={<Event />}>
-          <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>}/>
-          <Route path='two' element={<div>생일기념 쿠폰 받기</div>}/>
-        </Route>
-      </Routes>
+      {/* Suspense로 감싸면 로딩중 UI넣기 가능 */}
+      <Suspense fallback={<div>Loading!</div>}>
+        <Routes>
+          <Route path='/' element={<Item shoes={shoes} />}/>
+          <Route path='*' element={<div>없는페이지입니다.</div>}/>
+          <Route path='/detail/:id' element={<ItemDetail shoes={shoes} />}/>
+          <Route path='/cart' element={<Cart />}/>
+          {/* nested routed */}
+          <Route path='/event' element={<Event />}>
+            <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>}/>
+            <Route path='two' element={<div>생일기념 쿠폰 받기</div>}/>
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 } 
